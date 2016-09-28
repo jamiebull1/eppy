082dcaaf642d662b918cf8c56526cc9808799af5
Jamie Bull <jamie.bull@oco-carbon.com>
2016-09-28 02:41:23 +0200

    Catch import error importing eppy during setup.py
    This is because we try to import IDF before the dependencies have been
    installed, specifically `six` is required.

--- eppy/__init__.py
+++ eppy/__init__.py
@@ -1,4 +1,7 @@
 """version number"""
 __version__ = '0.5.31'
-
-from eppy.modeleditor import IDF
+try:
+    from eppy.modeleditor import IDF
+except ImportError:
+    # we hit this  line during setup.py before dependecies have been installed
+    pass
