3c3fcdb4f583044611e7cdc5051da449da49e132
jamiebull1 <jamiebull1@gmail.com>
2018-10-11 13:08:12 +0200

    Test for #i204 error on all platforms, and improve error reporting

--- eppy/tests/test_crash_bug.py
+++ eppy/tests/test_crash_bug.py
@@ -1,9 +1,11 @@
 import os
 import shutil
-from subprocess import CalledProcessError
+import sys
+
+import pytest
 
 from eppy.modeleditor import IDF
-from eppy.runner.run_functions import paths_from_version
+from eppy.runner.run_functions import paths_from_version, EnergyPlusRunError
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,18 +15,17 @@ IDD_FILES = os.path.join(RESOURCES_DIR, 'iddfiles')
 IDF_FILES = os.path.join(RESOURCES_DIR, 'idffiles')
 
 
+@pytest.mark.xfail
 def test_reproduce_i204():
-    iddfile = os.path.join(IDD_FILES, "Energy+V8_9_0.idd")
-    IDF.setiddname(iddfile)
+    """This test is not failing. Perhaps if we had the actual files it would."""
     idfname = os.path.join(IDF_FILES, "V8_9/AirCooledElectricChiller.idf")
     _, eplus_home = paths_from_version("8-9-0")
     epwfile = os.path.join(eplus_home, "WeatherData/USA_IL_Chicago-OHare.Intl.AP.725300_TMY3.epw")
     idf = IDF(idfname, epwfile)
-    output_dir = "test_dir"
     try:
-        idf.run(output_directory=output_dir)
-        assert False, "expected error not raised"
-    except CalledProcessError:
-        pass
+        with pytest.raises(EnergyPlusRunError):
+            idf.run(output_directory="test_dir", ep_version="8-9-0")
+    except Exception:
+        raise
     finally:
-        shutil.rmtree(output_dir, ignore_errors=True)
+        shutil.rmtree("test_dir", ignore_errors=True)
